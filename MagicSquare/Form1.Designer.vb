8eb4b5ef3dcbcdcb6aded95ac717ac8825bf6945
Ryan-Amaral <ryan_amaral@live.com>
2017-04-30 16:07:41 -0300

    initial

---
+++ MagicSquare/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormMagicSquare
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LabelEnterOdd = New System.Windows.Forms.Label()
+        Me.TextBoxOddNumber = New System.Windows.Forms.TextBox()
+        Me.ButtonGenerateMagicSquare = New System.Windows.Forms.Button()
+        Me.LabelSumOfFirstRowPrompt = New System.Windows.Forms.Label()
+        Me.LabelSumOfFirstRowValue = New System.Windows.Forms.Label()
+        Me.LabelSumOfFirstColumnPrompt = New System.Windows.Forms.Label()
+        Me.LabelSumOfFirstColumnValue = New System.Windows.Forms.Label()
+        Me.LabelSumOfDiagonalPrompt = New System.Windows.Forms.Label()
+        Me.LabelSumOfDiagonalValue = New System.Windows.Forms.Label()
+        Me.ButtonSave = New System.Windows.Forms.Button()
+        Me.TextBoxMagicSquare = New System.Windows.Forms.TextBox()
+        Me.SaveFileDialogMagicSquare = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'LabelEnterOdd
+        '
+        Me.LabelEnterOdd.AutoSize = True
+        Me.LabelEnterOdd.Location = New System.Drawing.Point(12, 9)
+        Me.LabelEnterOdd.Name = "LabelEnterOdd"
+        Me.LabelEnterOdd.Size = New System.Drawing.Size(113, 13)
+        Me.LabelEnterOdd.TabIndex = 0
+        Me.LabelEnterOdd.Text = "Enter an Odd Number:"
+        '
+        'TextBoxOddNumber
+        '
+        Me.TextBoxOddNumber.Location = New System.Drawing.Point(15, 25)
+        Me.TextBoxOddNumber.Name = "TextBoxOddNumber"
+        Me.TextBoxOddNumber.Size = New System.Drawing.Size(103, 20)
+        Me.TextBoxOddNumber.TabIndex = 1
+        '
+        'ButtonGenerateMagicSquare
+        '
+        Me.ButtonGenerateMagicSquare.Location = New System.Drawing.Point(15, 60)
+        Me.ButtonGenerateMagicSquare.Name = "ButtonGenerateMagicSquare"
+        Me.ButtonGenerateMagicSquare.Size = New System.Drawing.Size(165, 66)
+        Me.ButtonGenerateMagicSquare.TabIndex = 2
+        Me.ButtonGenerateMagicSquare.Text = "Generate Magic Square"
+        Me.ButtonGenerateMagicSquare.UseVisualStyleBackColor = True
+        '
+        'LabelSumOfFirstRowPrompt
+        '
+        Me.LabelSumOfFirstRowPrompt.AutoSize = True
+        Me.LabelSumOfFirstRowPrompt.Location = New System.Drawing.Point(12, 149)
+        Me.LabelSumOfFirstRowPrompt.Name = "LabelSumOfFirstRowPrompt"
+        Me.LabelSumOfFirstRowPrompt.Size = New System.Drawing.Size(100, 13)
+        Me.LabelSumOfFirstRowPrompt.TabIndex = 3
+        Me.LabelSumOfFirstRowPrompt.Text = "Sum of First Row is:"
+        '
+        'LabelSumOfFirstRowValue
+        '
+        Me.LabelSumOfFirstRowValue.AutoSize = True
+        Me.LabelSumOfFirstRowValue.Location = New System.Drawing.Point(118, 149)
+        Me.LabelSumOfFirstRowValue.Name = "LabelSumOfFirstRowValue"
+        Me.LabelSumOfFirstRowValue.Size = New System.Drawing.Size(0, 13)
+        Me.LabelSumOfFirstRowValue.TabIndex = 4
+        '
+        'LabelSumOfFirstColumnPrompt
+        '
+        Me.LabelSumOfFirstColumnPrompt.AutoSize = True
+        Me.LabelSumOfFirstColumnPrompt.Location = New System.Drawing.Point(12, 171)
+        Me.LabelSumOfFirstColumnPrompt.Name = "LabelSumOfFirstColumnPrompt"
+        Me.LabelSumOfFirstColumnPrompt.Size = New System.Drawing.Size(113, 13)
+        Me.LabelSumOfFirstColumnPrompt.TabIndex = 5
+        Me.LabelSumOfFirstColumnPrompt.Text = "Sum of First Column is:"
+        '
+        'LabelSumOfFirstColumnValue
+        '
+        Me.LabelSumOfFirstColumnValue.AutoSize = True
+        Me.LabelSumOfFirstColumnValue.Location = New System.Drawing.Point(131, 171)
+        Me.LabelSumOfFirstColumnValue.Name = "LabelSumOfFirstColumnValue"
+        Me.LabelSumOfFirstColumnValue.Size = New System.Drawing.Size(0, 13)
+        Me.LabelSumOfFirstColumnValue.TabIndex = 6
+        '
+        'LabelSumOfDiagonalPrompt
+        '
+        Me.LabelSumOfDiagonalPrompt.AutoSize = True
+        Me.LabelSumOfDiagonalPrompt.Location = New System.Drawing.Point(12, 193)
+        Me.LabelSumOfDiagonalPrompt.Name = "LabelSumOfDiagonalPrompt"
+        Me.LabelSumOfDiagonalPrompt.Size = New System.Drawing.Size(123, 13)
+        Me.LabelSumOfDiagonalPrompt.TabIndex = 7
+        Me.LabelSumOfDiagonalPrompt.Text = "Sum of Diagonal Row is:"
+        '
+        'LabelSumOfDiagonalValue
+        '
+        Me.LabelSumOfDiagonalValue.AutoSize = True
+        Me.LabelSumOfDiagonalValue.Location = New System.Drawing.Point(141, 193)
+        Me.LabelSumOfDiagonalValue.Name = "LabelSumOfDiagonalValue"
+        Me.LabelSumOfDiagonalValue.Size = New System.Drawing.Size(0, 13)
+        Me.LabelSumOfDiagonalValue.TabIndex = 8
+        '
+        'ButtonSave
+        '
+        Me.ButtonSave.Location = New System.Drawing.Point(15, 230)
+        Me.ButtonSave.Name = "ButtonSave"
+        Me.ButtonSave.Size = New System.Drawing.Size(163, 39)
+        Me.ButtonSave.TabIndex = 9
+        Me.ButtonSave.Text = "Save Result to File"
+        Me.ButtonSave.UseVisualStyleBackColor = True
+        '
+        'TextBoxMagicSquare
+        '
+        Me.TextBoxMagicSquare.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxMagicSquare.Location = New System.Drawing.Point(186, 18)
+        Me.TextBoxMagicSquare.Multiline = True
+        Me.TextBoxMagicSquare.Name = "TextBoxMagicSquare"
+        Me.TextBoxMagicSquare.Size = New System.Drawing.Size(373, 319)
+        Me.TextBoxMagicSquare.TabIndex = 10
+        '
+        'SaveFileDialogMagicSquare
+        '
+        Me.SaveFileDialogMagicSquare.DefaultExt = "txt"
+        Me.SaveFileDialogMagicSquare.Filter = "Text files (*.txt)|*.txt"
+        '
+        'FormMagicSquare
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(571, 353)
+        Me.Controls.Add(Me.TextBoxMagicSquare)
+        Me.Controls.Add(Me.ButtonSave)
+        Me.Controls.Add(Me.LabelSumOfDiagonalValue)
+        Me.Controls.Add(Me.LabelSumOfDiagonalPrompt)
+        Me.Controls.Add(Me.LabelSumOfFirstColumnValue)
+        Me.Controls.Add(Me.LabelSumOfFirstColumnPrompt)
+        Me.Controls.Add(Me.LabelSumOfFirstRowValue)
+        Me.Controls.Add(Me.LabelSumOfFirstRowPrompt)
+        Me.Controls.Add(Me.ButtonGenerateMagicSquare)
+        Me.Controls.Add(Me.TextBoxOddNumber)
+        Me.Controls.Add(Me.LabelEnterOdd)
+        Me.Name = "FormMagicSquare"
+        Me.Text = "Magic Square Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LabelEnterOdd As System.Windows.Forms.Label
+    Friend WithEvents TextBoxOddNumber As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonGenerateMagicSquare As System.Windows.Forms.Button
+    Friend WithEvents LabelSumOfFirstRowPrompt As System.Windows.Forms.Label
+    Friend WithEvents LabelSumOfFirstRowValue As System.Windows.Forms.Label
+    Friend WithEvents LabelSumOfFirstColumnPrompt As System.Windows.Forms.Label
+    Friend WithEvents LabelSumOfFirstColumnValue As System.Windows.Forms.Label
+    Friend WithEvents LabelSumOfDiagonalPrompt As System.Windows.Forms.Label
+    Friend WithEvents LabelSumOfDiagonalValue As System.Windows.Forms.Label
+    Friend WithEvents ButtonSave As System.Windows.Forms.Button
+    Friend WithEvents TextBoxMagicSquare As System.Windows.Forms.TextBox
+    Friend WithEvents SaveFileDialogMagicSquare As System.Windows.Forms.SaveFileDialog
+
+End Class
---
+++ MagicSquare/Form1.vb
@@ -0,0 +1,309 @@
+﻿Option Strict On
+
+Imports System.IO
+
+Public Class FormMagicSquare
+
+    'make magic square global to be accessed by save file dialog
+    Dim magicSquare(,) As Integer
+
+    'store if there are values in the magic square for saving
+    Dim isMagicSquareFilled As Boolean = False
+
+    ''' <summary>
+    ''' Generate a magic square from on odd number
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub ButtonGenerateMagicSquare_Click(sender As Object, e As EventArgs) Handles ButtonGenerateMagicSquare.Click
+
+        'check if the input is valid
+        If IsInputSmallOddInteger(TextBoxOddNumber) Then
+
+            'variables for the magic square, all zero based
+            Dim sideLength As Integer = Convert.ToInt16(TextBoxOddNumber.Text) - 1
+            Dim startPosRow As Integer = 0
+            Dim startPosColumn As Integer = Convert.ToInt16(Math.Ceiling(sideLength / 2))
+
+            'the highest value for the magic square
+            Dim highestValue As Integer = ((sideLength + 1) * (sideLength + 1))
+
+            'create a magic square
+            magicSquare = GenerateMagicSquare(sideLength, highestValue, startPosRow, startPosColumn)
+
+            'write the magic square to the textbox
+            DisplayMagicSquare(TextBoxMagicSquare)
+
+            'get and write the sums for row, column and diagonal
+            RowSum(LabelSumOfFirstRowValue)
+            ColumnSum(LabelSumOfFirstColumnValue)
+            DiagonalSum(LabelSumOfDiagonalValue)
+
+            'the magic square is filled and all other proceedures are done
+            isMagicSquareFilled = True
+
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' determine if the input from the user is appropriate
+    ''' </summary>
+    ''' <param name="tBox"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function IsInputSmallOddInteger(tBox As TextBox) As Boolean
+
+        Const MAX_DIGITS As Integer = 2
+
+        'check if the input is valid
+        If IsNumeric(tBox.Text) Then
+            If tBox.Text.Contains("."c) = False Then
+                If tBox.Text.Length <= MAX_DIGITS Then
+                    If Convert.ToInt16(tBox.Text) > 0 Then
+                        If (Convert.ToInt16(tBox.Text) Mod 2) <> 0 Then
+                            Return True
+                        Else
+                            MessageBox.Show("Please enter an odd number", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            Return False
+                        End If
+                    Else
+                        MessageBox.Show("Please enter a bigger number", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        Return False
+                    End If
+                Else
+                    MessageBox.Show("Please enter a smaller number", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return False
+                End If
+            Else
+                MessageBox.Show("Please enter a whole number", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return False
+            End If
+        Else
+            MessageBox.Show("Please enter a valid number", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+    End Function
+
+    ''' <summary>
+    ''' Creates a magic square with an odd amount of rows and columns
+    ''' </summary>
+    ''' <param name="sideLength"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function GenerateMagicSquare(sideLength As Integer, highestValue As Integer, startRow As Integer, startColumn As Integer) As Integer(,)
+
+        'store the values for magic square
+        Dim aMagicSquare(sideLength, sideLength) As Integer
+
+        'probes used for determining valid positions in the array
+        Dim currentRow As Integer = startRow
+        Dim currentColumn As Integer = startColumn
+
+        'store current value to know which number to put in the array, and to judge the end
+        Dim currentValue = 1
+
+        'for each iteration; fill the current value in the probe x and y coordinates of the array,
+        'then determine the coordinates for next loop through, then increment the current value,
+        'starting in top middle. Origin is in top left.
+        Do While currentValue <= highestValue
+
+            'fill in the current value to the array
+            aMagicSquare(currentRow, currentColumn) = currentValue
+
+            'decrement row and increment column
+            currentRow = IncrementWrap(currentRow, -1, sideLength)
+            currentColumn = IncrementWrap(currentColumn, 1, sideLength)
+
+            'determine if position is filled
+            If aMagicSquare(currentRow, currentColumn) <> 0 Then
+                'increment row twice and decrement column once
+                currentRow = IncrementWrap(currentRow, 2, sideLength)
+                currentColumn = IncrementWrap(currentColumn, -1, sideLength)
+            End If
+
+            'increment the current value
+            currentValue += 1
+
+        Loop
+
+        Return aMagicSquare
+
+    End Function
+
+    ''' <summary>
+    ''' Allows incrementing with wrap
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function IncrementWrap(currentIndex As Integer, totalIncrement As Integer, upperBound As Integer) As Integer
+
+        currentIndex += totalIncrement
+
+        If currentIndex > upperBound Then
+            'use mod to get remainder which is new index
+            currentIndex = currentIndex Mod (upperBound + 1)
+        ElseIf currentIndex < 0 Then
+            'convert index to positive and add to upper bounds
+            currentIndex = (currentIndex * -1) + upperBound
+            'mod the value to get amount above upperbounds
+            currentIndex = currentIndex Mod (upperBound + 1)
+            'subtract current index from upper bounds to get wrap in the right direction
+            currentIndex = upperBound - currentIndex
+        End If
+
+        Return currentIndex
+
+    End Function
+
+    ''' <summary>
+    ''' writes a magic square to a text box
+    ''' </summary>
+    ''' <param name="tBox"></param>
+    ''' <remarks></remarks>
+    Private Sub DisplayMagicSquare(tBox As TextBox)
+
+        'clear the text box
+        tBox.Text = vbNullString
+
+        'save the biggest amount of digits to space appropriately
+        Dim biggestNumberDigits As Integer = ((magicSquare.GetUpperBound(0) + 1) * (magicSquare.GetUpperBound(1) + 1)).ToString.Length()
+
+        'index through each row and column
+        For row = 0 To magicSquare.GetUpperBound(0)
+
+            For column = 0 To magicSquare.GetUpperBound(1)
+
+                tBox.Text &= magicSquare(row, column).ToString()
+
+                'add spaces after all but last in the column
+                If column < magicSquare.GetUpperBound(1) Then
+
+                    'add more spaces if has fewer digits than highest digits
+                    For digit = magicSquare(row, column).ToString.Length() To biggestNumberDigits - 1
+                        tBox.Text &= " "
+                    Next
+
+                    'always have atleast one space
+                    tBox.Text &= " "
+
+                End If
+
+            Next
+
+            'add a new line after each row except the last
+            If row < magicSquare.GetUpperBound(0) Then
+                tBox.Text &= vbCrLf
+            End If
+
+        Next
+
+    End Sub
+
+    ''' <summary>
+    ''' Returns the sum of all of the numbers in the first row of a magic square
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub RowSum(tBox As Label)
+
+        Dim sum As Integer = 0
+
+        For column = 0 To magicSquare.GetUpperBound(1)
+            sum += magicSquare(0, column)
+        Next
+
+        tBox.Text = sum.ToString()
+
+    End Sub
+
+    ''' <summary>
+    ''' Returns the sum of all of the numbers in the first column of a magic square
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub ColumnSum(tBox As Label)
+
+        Dim sum As Integer = 0
+
+        For row = 0 To magicSquare.GetUpperBound(0)
+            sum += magicSquare(row, 0)
+        Next
+
+        tBox.Text = sum.ToString()
+
+    End Sub
+
+
+    ''' <summary>
+    ''' Returns the sum of all of the numbers diagonally through a magic square
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub DiagonalSum(tBox As Label)
+
+        Dim sum As Integer = 0
+
+        For rowColumn = 0 To magicSquare.GetUpperBound(0)
+            sum += magicSquare(rowColumn, rowColumn)
+        Next
+
+        tBox.Text = sum.ToString()
+
+    End Sub
+
+    ''' <summary>
+    ''' Save the magic square to a text file
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub ButtonSave_Click(sender As Object, e As EventArgs) Handles ButtonSave.Click
+
+        If isMagicSquareFilled = True Then
+            'save to file if magic square has values
+            If SaveFileDialogMagicSquare.ShowDialog = Windows.Forms.DialogResult.OK Then
+                SaveToFile(SaveFileDialogMagicSquare.FileName)
+            End If
+        Else
+            MessageBox.Show("Please create a Magic Square first", "No Magic Square", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Writes the values of the magic square to a .txt file
+    ''' </summary>
+    ''' <param name="fileName"></param>
+    ''' <remarks></remarks>
+    Private Sub SaveToFile(fileName As String)
+
+        Dim writer As StreamWriter = File.CreateText(fileName)
+
+        'index through each row and column
+        For row = 0 To MagicSquare.GetUpperBound(0)
+
+            For column = 0 To MagicSquare.GetUpperBound(1)
+
+                writer.Write(magicSquare(row, column).ToString())
+
+                'add spaces after all but last in the column
+                If column < MagicSquare.GetUpperBound(1) Then
+                    writer.Write(",")
+                End If
+
+            Next
+
+            'add a new line after each row except the last
+            If row < MagicSquare.GetUpperBound(0) Then
+                writer.WriteLine()
+            End If
+
+        Next
+
+        MessageBox.Show("Results saved to " & fileName, "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        writer.Close()
+
+    End Sub
+
+End Class
